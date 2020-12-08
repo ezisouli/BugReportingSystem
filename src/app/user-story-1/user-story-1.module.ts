bf478c8946bccf138e51d1b3685121bafeac701e
unknown <evaggeliazisouli@gmail.com>
2020-12-08 18:38:27 +0200

    User Story 2

--- src/app/user-story-1/user-story-1.module.ts
+++ src/app/user-story-1/user-story-1.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Us1Component } from './us1/us1.component';
+import { Us2Component } from './us2/us2.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes : Routes = [
+  {
+    path:'buglist' , component : Us1Component
+  },
+  {
+    path: 'bugform' , component : Us2Component
+  }
+]
 
 @NgModule({
-  declarations: [Us1Component],
+  declarations: [Us1Component, Us2Component],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
   exports:[
-    Us1Component
+    Us1Component,
+    Us2Component
   ]
 })
 export class UserStory1Module { }
