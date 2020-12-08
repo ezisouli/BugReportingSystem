6c1c4a2f934fb5b508cfc299cc0d5482452bf7f0
Argiro Gerantoni <g.argyrw@gmail.com>
2020-12-08 21:29:06 +0200

    basic form stracture

--- src/app/user-story-1/us2/us2.component.ts
+++ src/app/user-story-1/us2/us2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-us2',
@@ -9,7 +10,31 @@ export class Us2Component implements OnInit {
 
   constructor() { }
 
+  form: FormGroup;
+  priority=['minor', 'major', 'critical'];
+  reporter=['QA', 'PO', 'DEV'];
+  status=['ready for testing', 'done', 'rejected'];
+
+
   ngOnInit(): void {
+    this.form= new FormGroup({
+      title: new FormControl(null, Validators.required),
+      description: new FormControl(null, Validators.required),
+      priority: new FormControl(null, Validators.required),
+      reporter: new FormControl(null, Validators.required),
+      status: new FormControl(null)
+    })
+    this.form.get('reporter').valueChanges.subscribe(value=>{
+      const priorityFormControl = this.form.get('status');
+      if (value==='QA') {
+        priorityFormControl.setValidators(Validators.required);
+      }
+      else{
+        priorityFormControl.clearAsyncValidators();
+      }
+      priorityFormControl.updateValueAndValidity();
+    })
   }
+  
 
 }
--- src/app/user-story-1/user-story-1.module.ts
+++ src/app/user-story-1/user-story-1.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { Us1Component } from './us1/us1.component';
 import { Us2Component } from './us2/us2.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 const routes : Routes = [
   {
-    path:'buglist' , component : Us1Component
+    path:'buglist' , component : Us1Component,
   },
   {
     path: 'bugform' , component : Us2Component
@@ -14,14 +19,18 @@ const routes : Routes = [
 ]
 
 @NgModule({
-  declarations: [Us1Component, Us2Component],
+  declarations: [Us1Component, Us2Component, NavComponent, FooterComponent, ContentComponent],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
   ],
   exports:[
     Us1Component,
-    Us2Component
+    Us2Component,
+    NavComponent,
+    FooterComponent,
+    ContentComponent
   ]
 })
 export class UserStory1Module { }
